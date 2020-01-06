60bdafd121497ea55f236b3b3b938aaabe474ee0
Hallvard Kristiansen <hal@wrk.no>
2020-01-06 15:56:39 +0100

    changed filenames to something more legible

--- wordpress/wp-content/themes/gigil-xjazz/searchform.php
+++
@@ -1,6 +0,0 @@
-<form class="row searchform" action="<?php echo home_url( '/' ); ?>" method="get">
-  <div class="col-xs-10">
-    <input type="search" name="s" id="searchfield" value="<?php the_search_query(); ?>">
-    <button type="submit" class="button">Search</button>
-  </div>
-</form>
--- wordpress/wp-content/themes/gigil/template-parts/snippet-banner.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The default template for displaying content of type banner
- */
-$link = (get_field('link_to_external_content') ? get_field('ext_link') : get_field('internal_link')); ?>
-<div class="banner">
-  <a href="<?php echo $link; ?>"><?php the_post_thumbnail('banner-image'); ?></a>
-  <?php echo '<button><a href="'.$link.'" rel="bookmark">'.get_field('banner_text').'</a></button>'; ?>
-</div>
